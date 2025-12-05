cdb3668cf290d9e03a96b0636baf8b95de6dfd14
default <natnael.debesay@stu.ocsb.ca>
2025-12-05 19:54:12 +0000

    added main.ts file

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-12-04
+ * @fileoverview This program counts down from 100 by 5s.
+ */
+
+// variables
+let number: number = 0;
+
+// print numbers
+for(
+  let number=100; number>=0; number -= 5) { 
+  console.log(number);
+}
+
+console.log("\nDone.");
